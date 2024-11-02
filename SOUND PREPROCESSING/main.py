08650286b54fe32dbb0fba255f1744f55a34daba
ankithreddys <ankithreddy653@gmail.com>
2024-11-02 09:40:52 -0400

    latest_02_11_2024

--- SOUND PREPROCESSING/main.py
+++ SOUND PREPROCESSING/main.py
@@ -1,6 +1,7 @@
 from custom_dataset import UrbanSoundDataset
 import torchaudio
 import torch
+from torch.utils.data import Dataset, DataLoader
 
 
 if __name__ == "__main__":
@@ -16,14 +17,16 @@ if __name__ == "__main__":
     print(f'Device using :{device}')
     new_sr = 8000
     max_samples = 16000
+
     
-    melspect = torchaudio.transforms.MelSpectrogram(sample_rate=new_sr,n_fft=1024,hop_length=512,n_mels=64)
-    usd = UrbanSoundDataset(annotations_file = annotations_file, 
-                            audio_dir = audio_dir,
-                            transformation = melspect,
-                            target_sample_rate = new_sr, 
-                            max_samples = max_samples, 
-                            device = device)
+    
+    #melspect = torchaudio.transforms.MelSpectrogram(sample_rate=new_sr,n_fft=1024,hop_length=512,n_mels=64)
+    # usd = UrbanSoundDataset(annotations_file = annotations_file, 
+    #                         audio_dir = audio_dir,
+    #                         transformation = melspect,
+    #                         target_sample_rate = new_sr, 
+    #                         max_samples = max_samples, 
+    #                         device = device)
     
 
     print(f'Total number of samples in dataset :{len(usd)}')
